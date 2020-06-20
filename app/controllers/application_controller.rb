86929561985dfad70af55f3dd4c558f38e4eb4c3
Randy <randyisbusy@gmail.com>
2020-06-19 23:35:45 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #updated for merge! Passed the IDE and now i'm on a local ENV
 end
